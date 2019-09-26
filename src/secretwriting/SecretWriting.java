dbc6052e780eefa7f57953ba3615dd3d5e8f270d
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-09-25 21:24:31 -0400

    Construct secretwriting package
    SecretWriting --> Cipher --> Caesar Shift

--- src/secretwriting/SecretWriting.java
+++ src/secretwriting/SecretWriting.java
@@ -2,7 +2,6 @@ package secretwriting;
 
 public class SecretWriting {
 	public static String define() {
-		return "
-				";
+		return "Secret writing is the science and art of obscuring an original message in some way, shape, or form for security of the contents of the messsage. ";
 	}
 }
