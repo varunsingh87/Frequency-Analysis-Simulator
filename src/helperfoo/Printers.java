762475052e86b169e60554a3753ed68cd30d6954
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-10-20 21:51:56 -0400

    Implement print methods - Printers.java
    public static void printStringArray(String str)
    public static void printStringArray(String str, String regex)
    public static void printCharArray(String str)
    public static void printCharArray(String str, String regex)

--- src/helperfoo/Printers.java
+++ src/helperfoo/Printers.java
@@ -8,5 +8,36 @@ package helperfoo;
  *
  */
 public class Printers {
+	public static void printStringArray(String str) {
+		System.out.print("{");
+		for (String element : str.split(" ")) {
+			System.out.print("\"" + element + "\", ");
+		}
+		System.out.println("}");
+	}
 
+	public static void printStringArray(String str, String regex) {
+		String[] asArr = str.split(regex);
+		System.out.print("{");
+		for (String element : asArr) {
+			System.out.print("\"" + element + "\", ");
+		}
+		System.out.println("}");
+	}
+	
+	public static void printCharArray(String str) {
+		System.out.print("{");
+		for (String element : str.split(" ")) {
+			System.out.print("'" + element + "', ");
+		}
+		System.out.println("}");
+	}
+	
+	public static void printCharArray(String str, String regex) {
+		System.out.print("{");
+		for (String element : str.split(" ")) {
+			System.out.print("'" + element + "', ");
+		}
+		System.out.println("}");
+	}
 }
