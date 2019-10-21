f9cb23ded9a87725d1188da5756dbd341ba7c9bf
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-10-20 21:53:32 -0400

    Create Frequencies class

---
+++ src/alphastats/Frequencies.java
@@ -0,0 +1,11 @@
+package alphastats;
+
+/**
+ * Contains methods based on Simon Singh's webpage, The Black Chamber:  
+ * {@link https://simonsingh.net/The_Black_Chamber/hintsandtips.html}
+ * @structure private method and public method
+ * @purpose applications that involve frequency analysis
+ */
+public final class Frequencies {
+
+}
